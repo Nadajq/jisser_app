1e7d349935627a274fd2bc5cb51a9872e46c463a
Nadajq <442301956@nu.edu.sa>
2025-02-05 16:47:48 +0300

    test

---
+++ lib/view/Specialist_info_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SpecialistInfoPage extends StatelessWidget {
+  const SpecialistInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Specialist Info Page")),
+    );
+  }
+}
---
+++ lib/view/center_info_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CenterInfoPage extends StatelessWidget {
+  const CenterInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+    body: Center(child: Text("center info page")),
+
+    );
+  }
+}
