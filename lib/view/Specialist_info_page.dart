e7d0ea3f00775dedb464a96d2b27968afd8fe013
Shahad MD <shahadmd800@gmail.com>
2025-02-06 21:46:49 +0300

    Update Specialist_info_page.dart

--- lib/view/Specialist_info_page.dart
+++ lib/view/Specialist_info_page.dart
@@ -1,136 +1,196 @@
 import 'package:flutter/material.dart';
 
-class SpecialistHomePage extends StatelessWidget {
+// الصفحة الرئيسية للطبيب المتخصص
+class SpecialistHomePage extends StatefulWidget {
   const SpecialistHomePage({Key? key}) : super(key: key);
+
+  @override
+  _SpecialistHomePageState createState() => _SpecialistHomePageState();
+}
+
+// الفئة المسؤولة عن الحالة والتفاعل مع واجهة المستخدم
+class _SpecialistHomePageState extends State<SpecialistHomePage> {
+  // متغيرات لحفظ الخيارات المحددة
+  String _selectedTime = "5:00 م";
+  String _selectedDuration = "30 دقيقة/150 ريال";
 
   @override
   Widget build(BuildContext context) {
     return Directionality(
-        textDirection: TextDirection.rtl, 
-        child: Scaffold(
-         // backgroundColor: Colors.grey[200],
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            elevation: 0,
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.blueAccent),
-              onPressed: () {},
-            ),
-            title: Center(
+      // لضبط اتجاه النص والعناصر من اليمين إلى اليسار
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.blueAccent),
+            onPressed: () {},
+          ),
+          title: Center(
             child: Image.asset(
-            'assets/jisserLogo.jpeg',
+              'assets/jisserLogo.jpeg', // شعار التطبيق
               width: 40,
               height: 40,
             ),
           ),
-          actions: [SizedBox(width: 48)], // توازن لموائمة الصورة في المنتصف
+          actions: [SizedBox(width: 48)], // لتوازن العناصر في الشريط العلوي
         ),
-          body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundImage:
-                          AssetImage('assets/specialist2.png'),
-                    ),
-                    SizedBox(width: 16),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'د. سهى هاني السعدي',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,color: Color(0xFF546E78)
-                          ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // محاذاة من اليمين
+            children: [
+              // صورة ومعلومات الطبيب
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/specialist2.png'),
+                  ),
+                  SizedBox(width: 16), // مسافة بين الصورة والنص
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'د. سهى هاني السعدي',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF546E78),
                         ),
-                        SizedBox(height: 8),
-                        Text(
-                          'أخصائي نفسي',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey[600],
-                          ),
+                      ),
+                      SizedBox(height: 8), // مسافة بين النصوص
+                      Text(
+                        'أخصائي نفسي',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey[600],
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 44),
-                InfoRow(
-                    icon: Icons.school,
-                    label: 'المؤهل العلمي',
-                    value: 'ماجستير تربية خاصة - USA'),
-                InfoRow(icon: Icons.history, label: 'الخبرة', value: '23 سنة'),
-                InfoRow(
-                    icon: Icons.menu_book,
-                    label: 'تقديم الجلسات',
-                    value: 'كتابية'),
-                SizedBox(height: 24),
-                Text(
-                  'وقت الجلسة',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SessionTimeButton(label: '5:00'),
-                    SessionTimeButton(label: '6:00'),
-                    SessionTimeButton(label: '8:00'),
-                  ],
-                ),
-                SizedBox(height: 24),
-                Text(
-                  'مدة الجلسة',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SessionDurationButton(label: '30 دقيقة/ 150 ريال'),
-                    SessionDurationButton(label: '1 ساعة/ 250 ريال'),
-                  ],
-                ),
-                const SizedBox(height: 80,),
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.indigo,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
                       ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 24), // مسافة بين الأقسام
+
+              // معلومات إضافية
+              InfoRow(
+                icon: Icons.school,
+                label: 'المؤهل العلمي',
+                value: 'ماجستير تربية خاصة - USA',
+              ),
+              InfoRow(icon: Icons.history, label: 'الخبرة', value: '23 سنة'),
+              InfoRow(
+                icon: Icons.menu_book,
+                label: 'تقديم الجلسات',
+                value: 'كتابية',
+              ),
+              SizedBox(height: 24), // مسافة بين الأقسام
+
+              // وقت الجلسة
+              Text(
+                'وقت الجلسة',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                // أزرار اختيار وقت الجلسة
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildRadioButton("5:00 م", _selectedTime, (value) {
+                    setState(() {
+                      _selectedTime = value!;
+                    });
+                  }),
+                  buildRadioButton("6:00 م", _selectedTime, (value) {
+                    // تغيير القيمة عند الاختيار
+                    setState(() {
+                      _selectedTime = value!;
+                    });
+                  }),
+                  buildRadioButton("8:00 م", _selectedTime, (value) {
+                    setState(() {
+                      _selectedTime = value!;
+                    });
+                  }),
+                ],
+              ),
+
+              SizedBox(height: 24), // مسافة بين الأقسام
+
+              // مدة الجلسة
+              Text(
+                'مدة الجلسة',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildRadioButton("30 دقيقة/150 ريال", _selectedDuration,
+                      (value) {
+                    setState(() {
+                      _selectedDuration = value!;
+                    });
+                  }),
+                  buildRadioButton("1 ساعة/250 ريال", _selectedDuration,
+                      (value) {
+                    setState(() {
+                      _selectedDuration = value!;
+                    });
+                  }),
+                ],
+              ),
+
+              SizedBox(height: 24), // مسافة بين الأقسام
+
+              // زر الحجز
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // إضافة الحجز
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 32, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20), // زوايا مستديرة
                     ),
-                    child: Text(
-                      'حجز الجلسة',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
+                  ),
+                  child: Text(
+                    'حجز الجلسة',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+// دالة لإنشاء زر اختيار
+  Widget buildRadioButton(
+      String text, String groupValue, Function(String?) onChanged) {
+    return Row(
+      children: [
+        Radio<String>(
+          value: text,
+          groupValue: groupValue, // القيمة الحالية للمجموعة
+          onChanged: onChanged, // الإجراء عند التغيير
+        ),
+        Text(text), // النص بجانب زر الاختيار
+      ],
+    );
   }
 }
-//We created a custom widget (InfoRow) to reuse a styled row in our UI, keeping the code clean .
+
+// ويدجت مخصصة لعرض معلومات الطبيب
 class InfoRow extends StatelessWidget {
   final IconData icon;
   final String label;
@@ -146,64 +206,23 @@ class InfoRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      padding: const EdgeInsets.symmetric(vertical: 8.0), // حواف عمودية
       child: Row(
         children: [
           Icon(icon, color: Colors.indigo),
           SizedBox(width: 8),
-          Text('$label: ',
+          Text('$label: ', // النص الرئيسي
               style: TextStyle(
                 fontWeight: FontWeight.bold,
               )),
           Expanded(
-            child: Text(value,
+            child: Text(value, // النص المرافق
                 style: TextStyle(
                   color: Colors.grey[700],
                 )),
           ),
         ],
       ),
-    );
-  }
-}
-
-class SessionTimeButton extends StatelessWidget {
-  final String label;
-
-  const SessionTimeButton({required this.label, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return OutlinedButton(
-      onPressed: () {},
-      style: OutlinedButton.styleFrom(
-        side: BorderSide(color: Colors.grey),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-      ),
-      child: Text(label, style: TextStyle(color:Color(0xFF071164))),
-    );
-  }
-}
-
-class SessionDurationButton extends StatelessWidget {
-  final String label;
-
-  const SessionDurationButton({required this.label, Key? key})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return OutlinedButton(
-      onPressed: () {},
-      style: OutlinedButton.styleFrom(
-        side: BorderSide(color: Colors.grey),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-      ),
-      child: Text(label, style: TextStyle(color: Color(0xFF071164))),
     );
   }
 }
