e0eae2614644b781936d691fb4fe7575b3320771
Shahad MD <shahadmd800@gmail.com>
2025-02-16 21:54:38 +0300

    admin_login_page.dart

--- lib/view/admin/admin_login_page.dart
+++ lib/view/admin/admin_login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:jisser_app/view/widgets/form_container_widget.dart';
 
-
+import 'manage_specialist_page.dart';
 
 class AdminLoginPage extends StatelessWidget {
   const AdminLoginPage({super.key});
