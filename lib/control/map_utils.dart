a708b01e72e2b84856e9f966f94a440b8dd36383
SYSTEMNasim <442301563@nu.edu.sa>
2025-03-01 21:32:00 +0300

    tast

--- lib/control/map_utils.dart
+++ lib/control/map_utils.dart
@@ -1,20 +1,20 @@
 /*import 'package:url_launcher/url_launcher.dart'; // استيراد مكتبة لفتح الروابط الخارجية
-//كلاس يحتوي على دالة لفتح الخرائط في جوجل ماب
+import 'package:url_launcher/url_launcher.dart';
+
 class MapUtils {
-  MapUtils._();// مُنشئ خاص لمنع إنشاء كائنات من هذا الكلاس
-  static Future<void> openMap(// دالة لفتح موقع معين في خرائط جوجل
-    String Latitude,
-    //double Longitude
-  ) async {
-   // إنشاء رابط لفتح الموقع باستخدام إحداثيات خط العرض
-    String googleMapUrl =
-        "https://www.google.com/maps/search/?api=1&query=$Latitude";
-        // التحقق مما إذا كان يمكن فتح الرابط
-    if (await canLaunch(googleMapUrl)) {
-      await launch(googleMapUrl);//فتح الرابط
+  MapUtils._(); // مُنشئ خاص لمنع إنشاء كائنات من هذا الكلاس
+
+  static Future<void> openMap(double latitude, double longitude) async {
+    final Uri googleMapUrl = Uri.parse(
+      "https://www.google.com/maps/search/?api=1&query=$latitude,$longitude",
+    );
+
+    if (await canLaunchUrl(googleMapUrl)) {
+      await launchUrl(googleMapUrl, mode: LaunchMode.externalApplication);
     } else {
-      throw 'Could not open the Map';// في حالة الفشل
+      throw 'Could not open the Map'; // في حالة الفشل
     }
   }
 }
+
 */
--- lib/view/center_info_page.dart
+++ lib/view/center_info_page.dart
@@ -3,128 +3,136 @@ import 'package:jisser_app/control/map_utils.dart';
 
 import '../model/center_model.dart'; // استيراد أداة فتح الخرائط
 
-
 class CenterInfoPage extends StatelessWidget {
   final Centers centers;
   const CenterInfoPage({super.key, required this.centers});
 
-   @override
-   Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back,
+                color: Colors.blueAccent, size: 28), // أيقونة الرجوع للخلف
+            onPressed: () {
+              // تنفيذ الرجوع للخلف
+              Navigator.pop(context);
+            },
+          ),
+          title: Center(
+            child: Image.asset(
+              'assets/jisserLogo.jpeg', // إدراج صورة الشعار في منتصف شريط التطبيق
+              width: 40,
+              height: 40,
+            ),
+          ),
+          actions: [SizedBox(width: 48)], // توازن لموائمة الصورة في المنتصف
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                centers.imagePath, //
+                width: double.infinity,
+                //height: 250,
+                // fit: BoxFit.cover,
+              ),
 
-     return Directionality(
-         textDirection: TextDirection.rtl,
-       child: Scaffold(
-       backgroundColor: Colors.white,
-       appBar: AppBar(
-         backgroundColor: Colors.white,
-         leading: IconButton(
-           icon: Icon(Icons.arrow_back, color: Colors.blueAccent, size: 28),// أيقونة الرجوع للخلف
-           onPressed: () {
-             // تنفيذ الرجوع للخلف
-             Navigator.pop(context);
-           },
-         ),
-         title: Center(
-           child: Image.asset(
-             'assets/jisserLogo.jpeg', // إدراج صورة الشعار في منتصف شريط التطبيق
-             width: 40,
-             height: 40,
-           ),
-         ),
-         actions: [SizedBox(width: 48)], // توازن لموائمة الصورة في المنتصف
-       ),
-       body: SingleChildScrollView(
-         child: Column(
-           children: [
-             Image.asset(
-               centers.imagePath, //
-               width: double.infinity,
+              // المحتوى النصي والمعلومات
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Directionality(
+                  // جعل النصوص من اليمين إلى اليسار
+                  textDirection: TextDirection.rtl,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        centers.name,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF071164),
+                        ),
+                      ),
 
+                      SizedBox(height: 8),
 
-             ),
+                      // الموقع
+                      Row(
+                        children: [
+                          Icon(Icons.location_on, color: Colors.grey),
+                          SizedBox(width: 5),
+                          Text(centers.location,
+                              style: TextStyle(
+                                fontSize: 16,
+                              )),
+                        ],
+                      ),
+                      SizedBox(height: 16),
 
-             // المحتوى النصي والمعلومات
-             Padding(
-               padding: const EdgeInsets.all(16.0),
-               child: Directionality(
-                 // جعل النصوص من اليمين إلى اليسار
-                 textDirection: TextDirection.rtl,
-                 child: Column(
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                   children: [
-                     Text(
-                       centers.name,
-                       style: TextStyle(
-                         fontSize: 24,
-                         fontWeight: FontWeight.bold,
-                         color: Color(0xFF071164),
-                       ),
-                     ),
+                      // وصف الجمعية
+                      Text(
+                        centers.description,
+                        textAlign: TextAlign.right,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 20),
 
-                     SizedBox(height: 8),
+                      // معلومات الاتصال
+                      Row(
+                        children: [
+                          Icon(Icons.email, color: Colors.indigo),
+                          SizedBox(width: 5),
+                          Text(centers.email, style: TextStyle(fontSize: 16)),
+                        ],
+                      ),
+                      SizedBox(height: 10),
 
-                     // الموقع
-                     Row(
-                       children: [
-                         Icon(Icons.location_on, color: Colors.grey),
-                         SizedBox(width: 5),
-                         Text(centers.location, style: TextStyle(fontSize: 16,)),
-                       ],
-                     ),
-                     SizedBox(height: 16),
+                      Row(
+                        children: [
+                          Icon(Icons.phone, color: Colors.indigo),
+                          SizedBox(width: 5),
+                          Text(centers.phone, style: TextStyle(fontSize: 16)),
+                        ],
+                      ),
 
-                     // وصف الجمعية
-                     Text(
-                       centers.description,
-                       textAlign: TextAlign.right,
-                       style: TextStyle(fontSize: 16),
-                     ),
-                     SizedBox(height: 20),
-
-                     // معلومات الاتصال
-                     Row(
-                       children: [
-                         Icon(Icons.email, color: Colors.indigo),
-                         SizedBox(width: 5),
-                         Text(centers.email, style: TextStyle(fontSize: 16)),
-                       ],
-                     ),
-                     SizedBox(height: 10),
-
-                     Row(
-                       children: [
-                         Icon(Icons.phone, color: Colors.indigo),
-                         SizedBox(width: 5),
-                         Text(centers.phone, style: TextStyle(fontSize: 16)),
-                       ],
-                     ),
-
-                     SizedBox(height: 10,),
-                     Padding(
-                       padding: const EdgeInsets.symmetric(horizontal: 20,),
-                       child: Align(
-                         alignment: Alignment.bottomLeft, // وضع الزر في الزاوية اليسرى
-                         child: ElevatedButton(
-                           style: ElevatedButton.styleFrom(
-                               backgroundColor: Colors.indigo,
-                               padding: EdgeInsets.symmetric(
-                                   horizontal: 10, vertical: 10)),
-                           onPressed: () {
-                              // MapUtils.openMap("https://g.co/kgs/KKyFCie");
-                           }, //فتح الموقع على الخرائط
-                           child: Icon(Icons.location_on_outlined,
-                               color: Color(0xffffffff), size: 50),
-                         ),
-                       ),
-                     )
-                   ],
-                 ),
-               ),
-             ),
-           ],
-         ),
-       ),
-       ),
-     );
-   }
- }
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                        ),
+                        child: Align(
+                          alignment: Alignment
+                              .bottomLeft, // وضع الزر في الزاوية اليسرى
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.indigo,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 10)),
+                            onPressed: () {
+                              MapUtils.openMap(
+                                  centers.latitude, centers.longitude);
+                            },
+                            child: Icon(Icons.location_on_outlined,
+                                color: Color(0xffffffff), size: 50),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
