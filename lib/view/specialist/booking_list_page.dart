b133916e1364709354b747697624fab8dc7d2cb8
Nadajq <442301956@nu.edu.sa>
2025-04-04 02:39:11 +0300

    test

--- lib/view/specialist/booking_list_page.dart
+++ lib/view/specialist/booking_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jisser_app/auth/auth_service.dart';
 import 'package:jisser_app/cubit/change_langauge_cubit.dart';
@@ -10,6 +11,8 @@ import 'package:jisser_app/view/chat_page/chat_page.dart';
 import 'package:jisser_app/view/specialist/schedule_page.dart';
 import 'package:jisser_app/view/user_login_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../widgets/custom_snack_bar.dart';
 
 class BookingListPage extends StatefulWidget {
   final Specialist specialist;
@@ -110,13 +113,31 @@ class _BookingListPageState extends State<BookingListPage> {
                       PopupMenuItem<int>(
                         value: 0,
                         child: ListTile(
-                          trailing:
-                              const Icon(Icons.mail, color: Colors.blueAccent),
-                          title: const Text("jisser@gmail.com",
-                              style: TextStyle(fontSize: 13)),
+                          leading: const Icon(Icons.mail, color: Colors.blueAccent),
+                          title: Column(
+                            children: [
+                              Text(S.of(context).contact_us,
+                                  style: const TextStyle(fontSize: 13)),
+                              GestureDetector(
+                                onTap: () async {
+                                  await Clipboard.setData(
+                                      const ClipboardData(text: "jisser@gmail.com"));
+
+                                  CustomSnackBar.snackBarwidget(
+                                      context: context,
+                                      color: Colors.green,
+                                      text: S.of(context).coping);
+                                },
+                                child: const Text("jisser@gmail.com",
+                                    style: TextStyle(fontSize: 13)),
+                              ),
+                            ],
+                          ),
                           visualDensity:
-                              const VisualDensity(horizontal: -4, vertical: -2),
-                          onTap: () => Navigator.pop(context),
+                          const VisualDensity(horizontal: -4, vertical: -2),
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
                         ),
                       ),
                       PopupMenuItem<int>(
